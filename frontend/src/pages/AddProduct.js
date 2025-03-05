9aa0543e6f0f07fd986db1b88bef548a7e77be0a
sivasaikakarla <kakarlasivasai043@gmail.com>
2025-03-05 21:05:07 +0530

    Image Commit

--- frontend/src/pages/AddProduct.js
+++ frontend/src/pages/AddProduct.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+// 
 function AddProduct() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
